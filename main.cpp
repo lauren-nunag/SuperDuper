27c87ddc510661b6730f64fd179fe5cee3690273
AayeshaIslam <92817486+AayeshaIslam@users.noreply.github.com>
2022-11-19 13:46:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,28 @@
+
+#include <iomanip>
+#include <iostream>
+#include <fstream> // For ifstream
+#include <sstream> // For istringstream
+#include <vector>
+#include <string>
+using namespace std;
+
+
 int main() {
-	"hello lauren";
+	//"hello lauren";
+	fstream file("sephora_dataset.csv", ios_base::in);
+	if (file.is_open()) {
+		string line;
+		getline(file, line);
+		while (getline(file, line)) {
+			stringstream stream(line);
+			string temp;
+			cout << line << endl;
+		
+		}
+	}
+	else {
+		cout << "file not open" << endl;
+	}
 	return 0;
 }
